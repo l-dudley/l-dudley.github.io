1b5a8b2d20a711feb1bb20efb681449efe25bf49
Logan Dudley <logan.dudley@stratusi.com>
2021-01-08 13:36:36 -0500

    set up react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
 import React from "react";
+import {
+    Route,
+    NavLink,
+    HashRouter
+} from "react-router-dom";
+import Home from "./Pages/Home";
+import Other from "./Pages/Other";
 
 const App = () => {
     return (
-        <div className="App">
-            <h1>Hello World!</h1>
-        </div>
+        <HashRouter>
+            <div className="App">
+                <h1>Hello World!</h1>
+            </div>  
+            <ul>
+                <li><NavLink exact to="/">Home</NavLink></li>
+                <li><NavLink to="/other">Other</NavLink></li>
+            </ul>
+            <div>
+                <Route exact path="/" component={Home}></Route>
+                <Route path="/other" component={Other}></Route>
+            </div>
+        </HashRouter>
     );
 }
 
---
+++ src/Pages/Other.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Other = () => {
+    return (
+        <div>Other Page</div>
+    )
+}
+
+export default Other;
